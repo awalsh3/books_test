6b72643ce311e4d8bd65011b8f4aa88f229ceb4f
Annie Walsh <annie@anyonething.co.uk>
2023-04-04 17:37:39 +0100

    Adds rating and word count to Book model

---
+++ db/migrate/20230404162752_add_word_count_to_book.rb
@@ -0,0 +1,5 @@
+class AddWordCountToBook < ActiveRecord::Migration[7.0]
+  def change
+    add_column :books, :word_count, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_121250) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_04_162752) do
   create_table "books", force: :cascade do |t|
     t.string "author_name"
     t.string "title"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
+    t.integer "word_count"
   end
 
 end
