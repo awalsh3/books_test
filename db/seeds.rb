09a09d874ed4c137248fecdf61c52e7b14f46dc4
Annie Walsh <annie@anyonething.co.uk>
2023-04-04 17:55:18 +0100

    Updates seeds file to add new field data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,28 @@ puts 'Creating books...'
 book1 = Book.new(
   author_name: 'J.K. Rowling',
   title: 'Harry Potter',
-  description: 'Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry. '
+  description: 'Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry. ',
+  rating: 5,
+  word_count: 76_944
 )
 book1.save
 
 book2 = Book.new(
   author_name: 'F. Scott Fitzgerald',
-  title: 'Great Gatsby',
-  description: "Set in the Jazz Age on Long Island, near New York City, the novel depicts first-person narrator Nick Carraway's interactions with mysterious millionaire Jay Gatsby and Gatsby's obsession to reunite with his former lover, Daisy Buchanan."
+  title: 'The Great Gatsby',
+  description: "Set in the Jazz Age on Long Island, near New York City, the novel depicts first-person narrator Nick Carraway's interactions with mysterious millionaire Jay Gatsby and Gatsby's obsession to reunite with his former lover, Daisy Buchanan.",
+  rating: 3,
+  word_count: 47_094
 )
+
 book2.save
 
 book3 = Book.new(
   author_name: 'Herman Melville',
   title: 'Moby-Dick',
-  description: "Moby-Dick; or, The Whale is an 1851 novel by American writer Herman Melville. The book is the sailor Ishmael's narrative of the maniacal quest of Ahab, captain of the whaling ship Pequod, for vengeance against Moby Dick, the giant white sperm whale that crippled him on the ship's previous voyage"
+  description: "Moby-Dick; or, The Whale is an 1851 novel by American writer Herman Melville. The book is the sailor Ishmael's narrative of the maniacal quest of Ahab, captain of the whaling ship Pequod, for vengeance against Moby Dick, the giant white sperm whale that crippled him on the ship's previous voyage",
+  rating: 4,
+  word_count: 209_117
 )
 book3.save
 
