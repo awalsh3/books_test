b76f76ff6ae2b6ab32ae07334e401066292f5fda
Annie Walsh <annie@anyonething.co.uk>
2023-04-04 17:40:40 +0100

    Adds validations for rating and word_count

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
-  validates :author_name, presence: true
+  validates :author_name, :rating, :word_count, presence: true
   validates :title, uniqueness: true
+  validates :rating, length: { in: 1..5 }
 end
