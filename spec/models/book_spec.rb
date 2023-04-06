5e8c7dd020a68356d1d55152899bed7065a3298c
Annie Walsh <annie@anyonething.co.uk>
2023-04-06 14:10:16 +0100

    Adds unique title and star rating validity tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -12,5 +12,28 @@ RSpec.describe Book, type: :model do
       )
       expect(book).to be_valid
     end
+
+    it 'is not valid without a unique title' do
+      book = Book.create(
+        author_name: 'Annie Walsh',
+        title: 'A memoir',
+        description: 'The amazing life story',
+        rating: 5,
+        word_count: 100_000
+      )
+      book1 = Book.new(
+        author_name: 'Annie Walsh',
+        title: 'A memoir',
+        description: 'The amazing life story',
+        rating: 5,
+        word_count: 100_000
+      )
+      expect(book1).to_not be_valid
+    end
+
+    it 'is not valid without a star rating between 1-5' do
+      book2 = Book.new(rating: 8)
+      expect(book2).to_not be_valid
+    end
   end
 end
