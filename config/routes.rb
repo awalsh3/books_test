0917235ead9aca1568faddf43e3fb336c095ae13
Annie Walsh <annie@anyonething.co.uk>
2023-04-06 16:45:18 +0100

    adds books index to route and api config

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  #
+  namespace 'api' do
+    namespace 'v1' do
+      root 'books#index'
+    end
+  end
 end
