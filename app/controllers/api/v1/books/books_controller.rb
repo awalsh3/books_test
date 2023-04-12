f266978acdce2a6b7ce12de1e6eadc5a9f1eb1f2
Annie Walsh <annie@anyonething.co.uk>
2023-04-12 14:45:06 +0100

    Revert "Adds index code"
    This reverts commit 64a8ad65cb8a07c1bbef7b93249f2218ca61ed6c.

--- app/controllers/api/v1/books/books_controller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::RestaurantsController < Api::V1::BaseController
-  def index
-    @books = policy_scope(Book)
-  end
-end
---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  module V1
+    class BooksController < ApplicationController
+      def index
+        @books = Book.order('created_at DESC')
+        render json: { status: 'SUCCESS', message: 'Loaded books', data: @books }, status: :ok
+      end
+    end
+  end
+end
