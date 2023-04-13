89e335236c26d9cf04f0e12a460c02e2fabdc7ae
Annie Walsh <annie@anyonething.co.uk>
2023-04-13 18:23:31 +0100

    Adds JSON structure to mock data

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,8 +2,39 @@ module Api
   module V1
     class BooksController < ApplicationController
       def index
-        @books = Book.order('created_at DESC')
-        render json: { status: 'SUCCESS', message: 'Loaded books', data: @books }, status: :ok
+        books = []
+
+        # Create an array to hold the serialized book data
+        serialized_books = []
+
+        # Iterate over each book record and format the data as a JSON API resource object
+        books.each do |book|
+          serialized_books << {
+            type: "books",
+            id: book.id.to_s,
+            attributes: {
+              author_name: book.author_name,
+              title: book.title,
+              description: book.description,
+              created_at: book.created_at.iso8601,
+              updated_at: book.updated_at.iso8601,
+              rating: book.rating,
+              word_count: book.word_count
+            }
+          }
+        end
+
+        # Format the response as a JSON API document
+        response = {
+          data: serialized_books,
+          meta: {
+            status: "SUCCESS",
+            message: "Loaded books"
+          }
+        }
+
+        # Render the response as JSON
+        render json: response.to_json
       end
     end
   end
