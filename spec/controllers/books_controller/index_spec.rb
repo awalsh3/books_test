97b38a77f4c46a8ed0607de395f0730f36e17dc4
Annie Walsh <annie@anyonething.co.uk>
2023-05-03 12:32:39 +0100

    Copy change and 200 to :success

--- spec/controllers/books_controller/index_spec.rb
+++ spec/controllers/books_controller/index_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'API V1 Books', type: :request do
   describe "GET '/api/v1/books'" do
-    it 'works!' do
+    it 'returns http success' do
       get api_v1_books_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:success)
     end
   end
 end
