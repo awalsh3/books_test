7b5d49b18e26842879db9a48ce7a084300f8d7eb
Annie Walsh <annie@anyonething.co.uk>
2023-05-03 12:29:45 +0100

    Unit test for index http get and response

--- spec/controllers/books_controller/index_spec.rb
+++ spec/controllers/books_controller/index_spec.rb
@@ -1,3 +1,10 @@
-RSpec.describe "/books", type: :request do
-  get "/api/v1/books"
+require 'rails_helper'
+
+RSpec.describe 'API V1 Books', type: :request do
+  describe "GET '/api/v1/books'" do
+    it 'works!' do
+      get api_v1_books_path
+      expect(response).to have_http_status(200)
+    end
+  end
 end
