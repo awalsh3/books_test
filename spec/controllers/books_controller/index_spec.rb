de85eb5d565de39207ec9253e1de52a409a85706
Annie Walsh <annie@anyonething.co.uk>
2023-05-05 11:24:51 +0100

    Adds json to expectations

--- spec/controllers/books_controller/index_spec.rb
+++ spec/controllers/books_controller/index_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'API V1 Books', type: :request do
-  describe "GET '/api/v1/books'" do
+  context "GET '/api/v1/books'" do
+    let(:json) { JSON.parse(response.body).deep_symbolize_keys }
     it 'returns http success' do
       get api_v1_books_path
       expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
+      expect(json).to eq({
+        data: [],
+        meta: {
+          message: 'Loaded books',
+          status: 'SUCCESS'
+        }
+      })
     end
   end
 end
