19690abc0caf5122a3f46cf8b813d8cb850c1717
Annie Walsh <annie@anyonething.co.uk>
2023-05-08 11:30:07 +0100

    Adds tests for edge cases: no books and returns all books"

--- spec/controllers/books_controller/index_spec.rb
+++ spec/controllers/books_controller/index_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'API V1 Books', type: :request do
         word_count: word_count
       )
     end
-    let(:author_name) { 'J.R.R. Tolkien'}
+    let(:author_name) { 'J.R.R. Tolkien' }
     let(:title) { 'The Hobbit' }
     let(:description) { 'the unforgettable story of Bilbo, a peace-loving hobbit, who embarks on a strange and magical adventure. A timeless classic.' }
     let(:word_count) { 250_000 }
@@ -29,6 +29,26 @@ RSpec.describe 'API V1 Books', type: :request do
         message: 'Loaded books',
         status: 'SUCCESS'
       })
+    end
+
+    it 'returns an empty array when there are no books' do
+      Book.destroy_all
+      get api_v1_books_path
+      expect(json[:data]).to be_empty
+    end
+
+    it 'returns all books in the response' do
+      Book.create(
+        author_name: 'George R.R. Martin',
+        title: 'A Game of Thrones',
+        description: 'Fight to the death of mupltiple families hungry for power',
+        rating: 5,
+        word_count: 600_000
+      )
+      get api_v1_books_path
+      expect(json[:data].size).to eq(2)
+      expect(json[:data][0][:attributes][:title]).to eq('The Hobbit')
+      expect(json[:data][1][:attributes][:title]).to eq('A Game of Thrones')
     end
   end
 end
