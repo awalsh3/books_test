e75c605494db1195d3f923a6cd283f08e59f5440
Annie Walsh <annie@anyonething.co.uk>
2023-07-07 12:19:21 +0100

    Refactors controller logic to use serialization service

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,37 +2,17 @@ module Api
   module V1
     class BooksController < ApplicationController
       def index
-        books = Book.all
+        render json: serialized_books
+      end
 
-        # Create an array to hold the serialized book data
-        serialized_books = []
+      private
 
-        # Iterate over each book record and format the data as a JSON API resource object
-        books.each do |book|
-          serialized_books << {
-            type: "books",
-            id: book.id.to_s,
-            attributes: {
-              author_name: book.author_name,
-              title: book.title,
-              description: book.description,
-              rating: book.rating,
-              word_count: book.word_count
-            }
-          }
-        end
+      def serialized_books
+        BookSerializer.serialize(books) # returns a json formatted string, as that is what serialization is in this context.
+      end
 
-        # Format the response as a JSON API document
-        response = {
-          data: serialized_books,
-          meta: {
-            status: "SUCCESS",
-            message: "Loaded books"
-          }
-        }
-
-        # Render the response as JSON
-        render json: response.to_json
+      def books
+        Books.all
       end
     end
   end
