8158b9dc68666466b43b781bf69e78a6ca80e897
Annie Walsh <annie@anyonething.co.uk>
2023-07-07 12:56:27 +0100

    Adds conditional to return empty data if there are no books

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -6,14 +6,23 @@ module Api
       end
 
       private
+        def serialized_books
+          if books.empty?
+            {
+              data: [],
+              meta: {
+                status: "SUCCESS",
+                message: "No books found"
+              }
+            }
+          else
+            BookSerializer.serialize(books) # returns a json formatted string, as that is what serialization is in this context.
+          end
+        end
 
-      def serialized_books
-        BookSerializer.serialize(books) # returns a json formatted string, as that is what serialization is in this context.
-      end
-
-      def books
-        Book.all
-      end
+        def books
+          Book.all
+        end
     end
   end
 end
