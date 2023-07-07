5c2944020907c044b88ab4c08935a98c9a9c7729
Annie Walsh <annie@anyonething.co.uk>
2023-07-07 12:11:33 +0100

    Style fix rubocop

--- app/services/book_serializer.rb
+++ app/services/book_serializer.rb
@@ -2,28 +2,28 @@ class BookSerializer
   def self.serialize(books)
     serialized_books = []
     books = Book.all
-     # add method for this edge case when there is only one book
-     unless books.count <= 1
-       books.each do |book|
-        serialized_books << {
-          type: "books",
-          id: book.id.to_s,
-          attributes: {
-            author_name: book.author_name,
-            title: book.title,
-            description: book.description,
-            rating: book.rating,
-            word_count: book.word_count
-          }
-        }
-       end
-      {
-        data: serialized_books,
-        meta: {
-          status: "SUCCESS",
-          message: "Loaded books"
+    # add method for this edge case when there is only one book
+    return if books.count <= 1
+
+    books.each do |book|
+      serialized_books << {
+        type: "books",
+        id: book.id.to_s,
+        attributes: {
+          author_name: book.author_name,
+          title: book.title,
+          description: book.description,
+          rating: book.rating,
+          word_count: book.word_count
         }
       }
     end
+    {
+      data: serialized_books,
+      meta: {
+        status: "SUCCESS",
+        message: "Loaded books"
+      }
+    }
   end
 end
