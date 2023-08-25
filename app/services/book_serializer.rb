80c58c164a74a7a70909728d877f3b4d6176f23a
Annie Walsh <annie@anyonething.co.uk>
2023-08-25 11:36:52 +0100

    Refactors Books Serializer to fewer method lines, more methods

--- app/services/book_serializer.rb
+++ app/services/book_serializer.rb
@@ -2,29 +2,38 @@
 
 # This is a serialization service class that handles serializing books.
 class BookSerializer
-  def self.serialize(books) # rubocop:todo Metrics/MethodLength
-    serialized_books = []
-    return if books.count < 1
+  # The main method to serialize the books
+  def self.serialize(books)
+    return unless books.any?
 
-    books.each do |book|
-      serialized_books << {
-        type: 'books',
-        id: book.id.to_s,
-        attributes: {
-          author_name: book.author_name,
-          title: book.title,
-          description: book.description,
-          rating: book.rating,
-          word_count: book.word_count
-        }
-      }
-    end
     {
-      data: serialized_books,
+      data: serialize_books(books),
       meta: {
         status: 'SUCCESS',
         message: 'Loaded books'
       }
+    }
+  end
+
+  def self.serialize_books(books)
+    books.map { |book| serialize_book(book) }
+  end
+
+  def self.serialize_book(book)
+    {
+      type: 'books',
+      id: book.id.to_s,
+      attributes: serialize_attributes(book)
+    }
+  end
+
+  def self.serialize_attributes(book)
+    {
+      author_name: book.author_name,
+      title: book.title,
+      description: book.description,
+      rating: book.rating,
+      word_count: book.word_count
     }
   end
 end
