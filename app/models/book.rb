3b51d347f6dee61203a4feccf7fbe828af89f0d6
Annie Walsh <annie@anyonething.co.uk>
2023-08-25 11:09:41 +0100

    add autocorrected fix

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-class Book < ApplicationRecord # rubocop:todo Style/Documentation
+# rubocop:todo Style/Documentation
+class Book < ApplicationRecord
   validates :author_name, :rating, :word_count, presence: true
   validates :title, uniqueness: true # rubocop:todo Rails/UniqueValidationWithoutIndex
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
