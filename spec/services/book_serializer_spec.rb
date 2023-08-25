7a07af11661dcd10e3d484dd44407ab898f9e63d
Annie Walsh <annie@anyonething.co.uk>
2023-08-25 15:38:27 +0100

    Refactors test to be ruby style compliant

--- spec/services/book_serializer_spec.rb
+++ spec/services/book_serializer_spec.rb
@@ -2,62 +2,50 @@
 
 require 'rails_helper'
 
-RSpec.describe BookSerializer, type: :model do # rubocop:todo Metrics/BlockLength
-  context 'serialization of book' do # rubocop:todo Metrics/BlockLength
-    let(:book1) do
-      Book.create(
-        author_name: 'Michael Bond',
-        title: 'Paddington Bear',
-        description: 'The adventures of a small toy bear, Paddington after the train station.',
-        rating: 5,
-        word_count: 500
-      )
-    end
-    let(:book2) do
-      Book.create(
-        author_name: 'A. A. Milne',
-        title: 'Winnie the Pooh',
-        description: 'A a good-natured, yellow-furred, honey-loving bear.',
-        rating: 4,
-        word_count: 600
-      )
-    end
+RSpec.describe BookSerializer, type: :model do
+  let(:book1) do
+    create(:book, author_name: 'Michael Bond', title: 'Paddington Bear',
+                  description: 'The adventures of a small toy bear, Paddington after the train station.')
+  end
+  let(:book2) do
+    create(:book, author_name: 'A. A. Milne', title: 'Winnie the Pooh',
+                  description: 'A a good-natured, yellow-furred, honey-loving bear.', rating: 4, word_count: 600)
+  end
 
+  context 'serialization of book' do
     let(:books) { [book1, book2] }
 
-    it 'returns serialized books' do # rubocop:todo Metrics/BlockLength
+    it 'returns serialized books' do
       serialized_result = BookSerializer.serialize(books)
-      puts "Here is the class: #{serialized_result.class}"
-      expect(serialized_result).to eq({
-                                        data: [
-                                          {
-                                            type: 'books',
-                                            id: '1',
-                                            attributes: {
-                                              author_name: book1.author_name,
-                                              title: book1.title,
-                                              description: book1.description,
-                                              rating: book1.rating,
-                                              word_count: book1.word_count
-                                            }
-                                          },
-                                          {
-                                            type: 'books',
-                                            id: '2',
-                                            attributes: {
-                                              author_name: book2.author_name,
-                                              title: book2.title,
-                                              description: book2.description,
-                                              rating: book2.rating,
-                                              word_count: book2.word_count
-                                            }
-                                          }
-                                        ],
-                                        meta: {
-                                          status: 'SUCCESS',
-                                          message: 'Loaded books'
-                                        }
-                                      })
+      expect(serialized_result).to eq(serialized_format)
     end
   end
+end
+
+def serialized_format
+  {
+    data: [book_data(book1, '1'), book_data(book2, '2')],
+    meta: {
+      status: 'SUCCESS',
+      message: 'Loaded books'
+    }
+  }
+end
+
+def book_data(book, id)
+  {
+    type: 'books',
+    id: id,
+    attributes: book_attributes(book)
+  }
+end
+
+def book_attributes(book)
+  {
+    author_name: book.author_name,
+    title: book.title,
+    description: book.description,
+    rating: book.rating,
+    word_count: book.word_count
+  }
 end
