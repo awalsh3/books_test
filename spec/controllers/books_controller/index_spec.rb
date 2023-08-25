c818243a998c1edc5739263685f23d8f5b29a8de
Annie Walsh <annie@anyonething.co.uk>
2023-08-25 17:14:18 +0100

    Beginning of refactoring books controller index

--- spec/controllers/books_controller/index_spec.rb
+++ spec/controllers/books_controller/index_spec.rb
@@ -2,67 +2,40 @@
 
 require 'rails_helper'
 
-RSpec.describe 'API V1 Books', type: :request do # rubocop:todo Metrics/BlockLength
-  context "GET '/api/v1/books'" do # rubocop:todo Metrics/BlockLength
-    let(:json) { JSON.parse(response.body).deep_symbolize_keys }
-    let!(:book) do
-      Book.create(
-        author_name: author_name,
-        title: title,
-        description: description,
-        rating: rating,
-        word_count: word_count
-      )
-    end
-    let(:author_name) { 'J.R.R. Tolkien' }
-    let(:title) { 'The Hobbit' }
-    let(:description) do
-      'A peace-loving hobbit, who embarks on a strange and magical adventure.'
-    end
-    let(:word_count) { 250_000 }
-    let(:rating) { 4 }
+RSpec.describe 'API V1 Books', type: :request do
+  let(:json) { JSON.parse(response.body).deep_symbolize_keys }
+  context "GET '/api/v1/books'" do
+    let!(:book) { create(:book) }
+    let!(:new_book) { create(:book, author_name: 'George R.R. Martin', title: 'Game of Thrones') }
 
     it 'returns http success' do
       get api_v1_books_path
       expect(response).to have_http_status(:success)
-      expect(response.status).to eq(200)
-      expect(json[:data].size).to eq(1)
-      expect(json[:meta]).to eq({
-                                  message: 'Loaded books',
-                                  status: 'SUCCESS'
-                                })
     end
 
     it 'returns all books in the response' do
-      Book.create(
-        author_name: 'George R.R. Martin',
-        title: 'A Game of Thrones',
-        description: 'Fight to the death of mupltiple families hungry for power',
-        rating: 5,
-        word_count: 600_000
-      )
       get api_v1_books_path
       expect(json[:data].size).to eq(2)
     end
+  end
 
-    context "No books" do
-      before do
-        Book.destroy_all
-      end
-      it 'returns an empty array when there are no books' do
-        get api_v1_books_path
-        expect(json[:data]).to be_empty
-      end
+  context 'No books' do
+    before do
+      Book.destroy_all
     end
+    it 'returns an empty array when there are no books' do
+      get api_v1_books_path
+      expect(json[:data]).to be_empty
+    end
+  end
 
-    context "Failure" do
-      before do
-        allow(BookSerializer).to receive(:serialize).and_raise(StandardError)
-      end
-      it 'it returns a 500 error' do
-        get api_v1_books_path
-        expect(response.status).to eq(500)
-      end
+  context 'Failure' do
+    before do
+      allow(BookSerializer).to receive(:serialize).and_raise(StandardError)
+    end
+    it 'it returns a 500 error' do
+      get api_v1_books_path
+      expect(response.status).to eq(500)
     end
   end
 end
