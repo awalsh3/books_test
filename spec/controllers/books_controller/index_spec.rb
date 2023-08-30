5fef5dd6746510f894174016dca45ee9b5bb70a8
Annie Walsh <annie@anyonething.co.uk>
2023-08-30 13:35:52 +0100

    Returns get path to it blocks

--- spec/controllers/books_controller/index_spec.rb
+++ spec/controllers/books_controller/index_spec.rb
@@ -3,31 +3,34 @@
 require 'rails_helper'
 
 RSpec.describe 'API V1 Books', type: :request do
-  before do
-    get api_v1_books_path
-  end
   let(:json) { JSON.parse(response.body).deep_symbolize_keys }
 
   context "GET '/api/v1/books'" do
     let!(:book) { create(:book) }
 
     it 'returns http success' do
+      get api_v1_books_path
       expect(response).to have_http_status(:success)
     end
   end
 
   context 'No books' do
-    it 'returns an empty array when there are no books' do
+    it 'returns an empty arrays when there are no books' do
       Book.destroy_all
+      get api_v1_books_path
       expect(json[:data]).to be_empty
     end
   end
 
   context 'Failure' do
+    let!(:book) { create(:book) } # Ensuring that at least one book exists.
+
     before do
       allow(BookSerializer).to receive(:serialize).and_raise(StandardError)
     end
+
     it 'it returns a 500 error' do
+      get api_v1_books_path
       expect(response.status).to eq(500)
     end
   end
