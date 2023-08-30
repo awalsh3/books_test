66a82425270ddf4e5c80839429e71df32ce8af30
Annie Walsh <annie@anyonething.co.uk>
2023-08-30 12:56:50 +0100

    Removes a redundant test about response size, removes a before do block only used once and adds a block for calling the path before each test

--- spec/controllers/books_controller/index_spec.rb
+++ spec/controllers/books_controller/index_spec.rb
@@ -3,28 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe 'API V1 Books', type: :request do
+  before do
+    get api_v1_books_path
+  end
   let(:json) { JSON.parse(response.body).deep_symbolize_keys }
+
   context "GET '/api/v1/books'" do
     let!(:book) { create(:book) }
-    let!(:new_book) { create(:book, author_name: 'George R.R. Martin', title: 'Game of Thrones') }
 
     it 'returns http success' do
-      get api_v1_books_path
       expect(response).to have_http_status(:success)
-    end
-
-    it 'returns all books in the response' do
-      get api_v1_books_path
-      expect(json[:data].size).to eq(2)
     end
   end
 
   context 'No books' do
-    before do
-      Book.destroy_all
-    end
     it 'returns an empty array when there are no books' do
-      get api_v1_books_path
+      Book.destroy_all
       expect(json[:data]).to be_empty
     end
   end
@@ -34,7 +28,6 @@ RSpec.describe 'API V1 Books', type: :request do
       allow(BookSerializer).to receive(:serialize).and_raise(StandardError)
     end
     it 'it returns a 500 error' do
-      get api_v1_books_path
       expect(response.status).to eq(500)
     end
   end
