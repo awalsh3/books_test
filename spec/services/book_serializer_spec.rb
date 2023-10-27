93f2d354b2c2a70b79ab79f99d0530f3348afe9f
Annie Walsh <annie@anyonething.co.uk>
2023-10-27 13:26:38 +0100

    Refactor where the hash starts

--- spec/services/book_serializer_spec.rb
+++ spec/services/book_serializer_spec.rb
@@ -13,26 +13,31 @@ RSpec.describe BookSerializer, type: :model do
   end
 
   let(:expected_serialized_result) do
-    array_including(
-      a_hash_including(
-        type: 'books',
-        id: be_an_instance_of(String),
-        attributes: a_hash_including(
-          author_name: be_an_instance_of(String),
-          title: be_an_instance_of(String),
-          description: be_an_instance_of(String),
-          rating: be_an_instance_of(Integer).or(be_nil),
-          word_count: be_an_instance_of(Integer).or(be_nil)
+    {
+      data: array_including(
+        a_hash_including(
+          type: 'books',
+          id: be_an_instance_of(String),
+          attributes: a_hash_including(
+            author_name: be_an_instance_of(String),
+            title: be_an_instance_of(String),
+            description: be_an_instance_of(String),
+            rating: be_an_instance_of(Integer).or(be_nil),
+            word_count: be_an_instance_of(Integer).or(be_nil)
+          )
         )
       )
-    )
+    }
   end
 
   let(:meta_result) do
-    a_hash_including(
-      status: 'SUCCESS',
-      message: 'Loaded books'
-    )
+    {
+      meta:
+        a_hash_including(
+          status: 'SUCCESS',
+          message: 'Loaded books'
+        )
+    }
   end
 
   context 'serialization of book' do
@@ -40,12 +45,12 @@ RSpec.describe BookSerializer, type: :model do
 
     it 'returns serialized books with correct structure' do
       serialized_result = BookSerializer.serialize(books)
-      expect(serialized_result).to include(data: expected_serialized_result)
+      expect(serialized_result).to include(expected_serialized_result)
     end
 
     it 'returns serialized books with correct meta message' do
       serialized_result = BookSerializer.serialize(books)
-      expect(serialized_result).to include(meta: meta_result)
+      expect(serialized_result).to include(meta_result)
     end
   end
 end
